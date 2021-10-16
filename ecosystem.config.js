c64861f45ff566528ca9e31a14c70419ea0b10bd
Chonwai <chonwaiun@gmail.com>
2021-10-17 03:40:53 +0800

    Update: Updated the ecosystem.config.js.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,10 @@ module.exports = {
             name: 'HumanCamYolo',
             instances: 1, // Or a number of instances
             cmd: "object_tracker.py",
-            args: "arg1 arg2 arg3 arg4 arg5 arg6 arg7",
+            args: "-weights './checkpoints/yolov4-tiny-416/' -model yolov4 -tiny True -video 'rtsp://admin:a1111111@192.168.14.248/Streaming/Channels/2' -output './data/outputs/cam2.mp4' -dont_show True",
             interpreter: 'python3',
             watch: true,
-            // autorestart: true,
+            autorestart: true,
         }
     ]
 };
