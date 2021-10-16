92a1ba163936f3d67f514c76d7219beff28ddca9
Chonwai <chonwaiun@gmail.com>
2021-10-17 02:56:42 +0800

    Add: Added the args on ecosystem.config.js.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
         {
             name: 'HumanCamYolo',
             instances: 1, // Or a number of instances
-            cmd: "object_tracker.py --weights='checkpoints/yolov4-tiny-416/' --model=yolov4 --tiny --video='rtsp://admin:a1111111@192.168.14.248/Streaming/Channels/2' --output='./data/outputs/cam2.mp4' --dont_show=true",
+            cmd: "object_tracker.py",
+            args: "--weights='checkpoints/yolov4-tiny-416/' --model=yolov4 --tiny --video='rtsp://admin:a1111111@192.168.14.248/Streaming/Channels/2' --output='./data/outputs/cam2.mp4' --dont_show=true",
             interpreter: 'python3',
             watch: true,
             autorestart: true,
