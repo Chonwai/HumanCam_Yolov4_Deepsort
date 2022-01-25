e9e51d380ba81bf4be12f39ad1b0a3446fd8fbf8
Chonwai <chonwaiun@gmail.com>
2022-01-26 01:50:52 +0800

    Update: Updated the Dockerfile and main code.

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,21 @@
-FROM python:3.7
+# FROM python:3.7
+# FROM nvidia/cuda:11.1.1-cudnn8-devel-ubuntu20.04
+FROM yssong67/cuda-opencv:112-810-440-1804
+ENV DEBIAN_FRONTEND noninteractive
 
 COPY requirements.txt .
 
-RUN pip install -r requirements.txt
-RUN pip install opencv-python-headless
+RUN apt-get update -y
 
-# RUN curl -sL https://deb.nodesource.com/setup_14.x | bash -
-# RUN apt-get install nodejs
-# RUN node -v
-# RUN npm install pm2 -g
+# RUN apt-get -y install python3.9
+RUN apt-get -y install python3-pip
+
+RUN pip install opencv-python-headless
+RUN pip install -r requirements.txt
+RUN pip install pyparsing==2.4.2
+
+RUN rm /usr/bin/python
+RUN ln -s /usr/bin/python3 /usr/bin/python
 
 ENV YOLO_ROOT /yolo
 RUN mkdir $YOLO_ROOT
@@ -17,4 +24,3 @@ WORKDIR /yolo
 COPY . .
 
 # ENTRYPOINT ["bash", "entrypoint.sh"]
-# CMD pm2-runtime start ecosystem.config.js
--- object_tracker.py
+++ object_tracker.py
@@ -1,3 +1,8 @@
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    tf.config.set_visible_devices(physical_devices[0:1], 'GPU')
 import zmq
 import json
 from datetime import datetime
@@ -22,14 +27,8 @@ from tools import generate_detections as gdet
 from utils.utils import Utils
 from utils.object import ObjectUtils
 import os
-import tensorflow as tf
 from dotenv import load_dotenv
 load_dotenv()
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
-    tf.config.set_visible_devices(physical_devices[0:1], 'GPU')
-
 
 # comment out below line to enable tensorflow logging outputs
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
